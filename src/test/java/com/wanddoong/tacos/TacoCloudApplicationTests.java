58c4d0fd9035f28cb669344ebbd1e75454ba2734
wanddoong <wanddoong@gmail.com>
2020-07-03 20:18:44 +0900

    add default application & controller

---
+++ src/test/java/com/wanddoong/tacos/TacoCloudApplicationTests.java
@@ -0,0 +1,11 @@
+package com.wanddoong.tacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TacoCloudApplicationTests {
+    @Test
+    public void contextLoads() {
+    }
+}
