80140e168d4a03114f02814958269496dfe8de5e
wanddoong <wanddoong@gmail.com>
2020-07-05 16:28:11 +0900

    add validation

--- src/main/java/com/wanddoong/tacos/web/DesignTacoController.java
+++ src/main/java/com/wanddoong/tacos/web/DesignTacoController.java
@@ -5,10 +5,12 @@ import com.wanddoong.tacos.Taco;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -51,9 +53,12 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processDesign(Taco design) {
-        log.info("Process design: " + design);
+    public String processDesign(@Valid Taco design, Errors errors) {
+        if(errors.hasErrors()) {
+            return "design";
+        }
 
+        log.info("Process design: " + design);
         return "redirect:/orders/current";
     }
 
